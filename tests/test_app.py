521dff20f03b7e715ae2c0f38d941caace0c506b
Thomas Thorogood <goodtom@uw.edu>
2020-12-14 14:02:16 -0800

    Fix name of health endpoint to match the kubernetes config (#6)

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,12 +3,12 @@ def test_get_index(client):
 
 
 def test_get_health(client):
-    response = client.get("/healthz")
+    response = client.get("/health")
     assert response.status_code == 200
     assert response.json["ready"] is False
 
 
 def test_get_ready(client):
-    response = client.get("/healthz?ready")
+    response = client.get("/health?ready")
     assert response.status_code == 200
     assert response.json["ready"] is True
