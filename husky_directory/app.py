753c2fb8206e2d3b24a1b995296b40e7823f3279
Tom Thorogood <tom@tomthorogood.com>
2021-02-10 17:04:56 -0800

    Support rendering error responses from search requests
    - Refactors to include an "error_display" template
    - Handles exceptions gracefully for users, instead of just crashing with a white screen

--- husky_directory/app.py
+++ husky_directory/app.py
@@ -65,6 +65,7 @@ class AppInjectorModule(Module):
     @staticmethod
     def register_jinja_filters(app: Flask):
         """You can define jinja filters here in order to make them available in our jinja templates."""
+
         @app.template_filter()
         def titleize(text):
             """
