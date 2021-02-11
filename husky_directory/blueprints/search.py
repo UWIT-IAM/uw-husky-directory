7672ac1d1fd90b1c7631d1fc79ca5fd8fa019738
Tom Thorogood <tom@tomthorogood.com>
2021-02-10 17:04:56 -0800

    Support rendering error responses from search requests
    - Refactors to include an "error_display" template
    - Handles exceptions gracefully for users, instead of just crashing with a white screen

--- husky_directory/blueprints/search.py
+++ husky_directory/blueprints/search.py
@@ -2,6 +2,7 @@ from logging import Logger
 
 from flask import Blueprint, Request, jsonify, render_template
 from injector import inject, singleton
+from pydantic import ValidationError
 
 from husky_directory.models.search import SearchDirectoryInput
 from husky_directory.services.search import DirectorySearchService
@@ -24,7 +25,18 @@ class SearchBlueprint(Blueprint):
             request_output.dict(by_alias=True, exclude_none=True, exclude_unset=True)
         )
 
-    def render(self, request: Request, service: DirectorySearchService):
-        return render_template(
-            "index.html", search_result=self.search(request, service).get_json()
-        )
+    def render(self, request: Request, service: DirectorySearchService, logger: Logger):
+        try:
+            return render_template(
+                "index.html", search_result=self.search(request, service).get_json()
+            )
+        except ValidationError as e:
+            # By default, the app just returns a BadRequest error, which is fine for
+            # an API context, but not so fine when we want to display something
+            # meaningful. Since we want to render the view, we pass the error context.
+            reportable_error = e.errors()[0]
+            return render_template("index.html", error=reportable_error), 401
+        except Exception as e:
+            logger.exception(str(e))
+            return render_template("index.html", error={'msg': "Something unexpected happened. Please try again or "
+                                                               "email help@uw.edu describing your problem."}), 500
