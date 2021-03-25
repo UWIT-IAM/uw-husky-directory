04bbf91b926d765cc2aa937770fa412766038bd6
Thomas Thorogood <goodtom@uw.edu>
2021-03-25 14:44:09 -0700

    Fix local sessions and add test to ensure session preservation (#46)
    - Completes the session setup by ensuring the rendering context is including session context
    - Refactor some HTML validation into a friendly `HTMLValidator` interface
    - Refactor `test_search_blueprint.py` to use the new `HTMLValidator`
    - Add `test_user_stays_logged_in` to ensure session preservation
    
    Again, this only covers file system caching. This _should_ now work in a deployed instance, as long as there is only one instance in the environment (as is the case for dev ).

--- tests/blueprints/test_search_blueprint.py
+++ tests/blueprints/test_search_blueprint.py
@@ -2,14 +2,13 @@ import re
 from unittest import mock
 
 import pytest
-from bs4 import BeautifulSoup
 
 from husky_directory.services.pws import PersonWebServiceClient
 
 
 class TestSearchBlueprint:
     @pytest.fixture(autouse=True)
-    def initialize(self, client, mock_people, injector):
+    def initialize(self, client, mock_people, injector, html_validator):
         self.pws_client = injector.get(PersonWebServiceClient)
         self.flask_client = client
         self.mock_people = mock_people
@@ -21,16 +20,7 @@ class TestSearchBlueprint:
             self.mock_people.contactable_person
         )
         self.mock_get_next.return_value = mock_people.as_search_output()
-
-    def assert_tag_with_text(self, html: BeautifulSoup, tag_name: str, text: str):
-        for element in html.find_all(tag_name):
-            for word in text.split():
-                if word not in element.text:
-                    continue
-            return
-        raise AssertionError(
-            f"Could not find any <{tag_name}> tags with text matching {text}"
-        )
+        self.html_validator = html_validator
 
     def test_json_success(self):
         response = self.flask_client.get("/search?name=foo")
@@ -52,55 +42,68 @@ class TestSearchBlueprint:
     def test_render_summary_success(self):
         response = self.flask_client.post("/search", data={"name": "foo"})
         assert response.status_code == 200
-        html = BeautifulSoup(response.data, "html.parser")
-        results_table = html.find("table", summary="results")
         profile = self.mock_people.contactable_person
-        self.assert_tag_with_text(results_table, "td", profile.display_name)
-        self.assert_tag_with_text(
-            results_table,
-            "td",
-            profile.affiliations.employee.directory_listing.phones[0],
-        )
-        self.assert_tag_with_text(
-            results_table,
-            "td",
-            profile.affiliations.employee.directory_listing.emails[0],
-        )
+        with self.html_validator.validate_response(response):
+            with self.html_validator.scope("table", summary="results"):
+                self.html_validator.assert_has_tag_with_text(
+                    "td",
+                    profile.affiliations.employee.directory_listing.phones[0],
+                )
+                self.html_validator.assert_has_tag_with_text(
+                    "td",
+                    profile.affiliations.employee.directory_listing.emails[0],
+                )
 
     def test_render_full_success(self):
         response = self.flask_client.post(
             "/search", data={"name": "foo", "length": "full"}
         )
-        html = BeautifulSoup(response.data, "html.parser")
-        listing = html.find("ul", class_="dir-listing")
         profile = self.mock_people.contactable_person
-        self.assert_tag_with_text(
-            listing, "li", profile.affiliations.employee.directory_listing.emails[0]
-        )
-        self.assert_tag_with_text(
-            listing, "li", profile.affiliations.employee.directory_listing.phones[0]
-        )
-        self.assert_tag_with_text(html, "h3", profile.display_name)
+        with self.html_validator.validate_response(response):
+            self.html_validator.assert_has_tag_with_text("h3", profile.display_name)
+            with self.html_validator.scope("ul", class_="dir-listing"):
+                self.html_validator.assert_has_tag_with_text(
+                    "li", profile.affiliations.employee.directory_listing.emails[0]
+                )
+                self.html_validator.assert_has_tag_with_text(
+                    "li", profile.affiliations.employee.directory_listing.phones[0]
+                )
 
     def test_render_no_results(self):
         self.mock_list_persons.return_value = self.mock_people.as_search_output()
         response = self.flask_client.post("/search", data={"name": "foo"})
-        html = BeautifulSoup(response.data, "html.parser")
-        assert not html.find("table", summary="results")
-        assert html.find(string=re.compile("No matches for"))
-        self.assert_tag_with_text(html, "b", 'Name is "foo"')
+        with self.html_validator.validate_response(response) as html:
+            assert not html.find("table", summary="results")
+            assert html.find(string=re.compile("No matches for"))
+            self.html_validator.assert_has_tag_with_text("b", 'Name is "foo"')
 
     def test_render_invalid_box_number(self):
         response = self.flask_client.post("/search", data={"box_number": "abcdef"})
-        html = BeautifulSoup(response.data, "html.parser")
-        assert not html.find("table", summary="results")
-        assert html.find(string=re.compile("Encountered error"))
         assert response.status_code == 400
-        self.assert_tag_with_text(html, "b", "invalid mailbox number")
+        with self.html_validator.validate_response(response) as html:
+            assert not html.find("table", summary="results")
+            assert html.find(string=re.compile("Encountered error"))
+            self.html_validator.assert_has_tag_with_text("b", "invalid mailbox number")
 
     def test_render_unexpected_error(self):
         self.mock_list_persons.side_effect = RuntimeError
         response = self.flask_client.post("/search", data={"box_number": "123456"})
-        html = BeautifulSoup(response.data, "html.parser")
-        assert response.status_code == 500
-        self.assert_tag_with_text(html, "b", "Something unexpected happened")
+        with self.html_validator.validate_response(response):
+            self.html_validator.assert_has_tag_with_text(
+                "b", "Something unexpected happened"
+            )
+
+    def test_user_stays_logged_in(self):
+        with self.html_validator.validate_response(self.flask_client.get("/")):
+            self.html_validator.assert_not_has_student_search_options()
+            self.html_validator.assert_has_sign_in_link()
+
+        with self.html_validator.validate_response(
+            self.flask_client.get("/saml/login", follow_redirects=True)
+        ):
+            self.html_validator.assert_has_student_search_options()
+            self.html_validator.assert_not_has_sign_in_link()
+
+        with self.html_validator.validate_response(self.flask_client.get("/")):
+            self.html_validator.assert_has_student_search_options()
+            self.html_validator.assert_not_has_sign_in_link()
