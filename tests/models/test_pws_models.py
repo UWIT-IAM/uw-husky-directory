777bb9176c52a2f591dd482c8f76e028f1e0e9a6
Tom Thorogood <tom@tomthorogood.com>
2021-04-19 10:17:07 -0700

    [EDS-576] ensure all human names are normalized on import from PWS

--- tests/models/test_pws_models.py
+++ tests/models/test_pws_models.py
@@ -1,4 +1,27 @@
-from husky_directory.models.pws import PersonOutput
+from husky_directory.models.pws import NamedIdentity, PersonOutput
+
+
+def test_name_formatting():
+    identity = NamedIdentity(
+        display_name="ALOE VERA",
+        registered_name="aloe VERA",
+        registered_first_middle_name="Aloe A. Vera",
+        registered_surname="VERA",
+        preferred_first_name="Aloe",
+        preferred_middle_name="A.",
+        preferred_last_name="VERA",
+    )
+
+    expected = {
+        "display_name": "Aloe Vera",
+        "registered_name": "Aloe Vera",
+        "registered_first_middle_name": "Aloe A. Vera",
+        "registered_surname": "Vera",
+        "preferred_first_name": "Aloe",
+        "preferred_middle_name": "A.",
+        "preferred_last_name": "Vera",
+    }
+    assert identity.dict() == expected
 
 
 def test_person_output_href():
