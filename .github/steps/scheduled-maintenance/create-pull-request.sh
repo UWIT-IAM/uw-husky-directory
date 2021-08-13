e254c4ac5ed5cb7eb284fe2fd0a0758cec0cd219
Tom Thorogood <tom@tomthorogood.com>
2021-08-13 12:20:41 -0700

    wip

---
+++ .github/steps/scheduled-maintenance/create-pull-request.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+pr_body="Beep boop! I'm a bot, here to make sure your dependencies are up to date! "
+pr_body+="Everything looks good, I just need your approval to merge this "
+pr_body+="change in to your main branch!"
+
+source $BUILD_SCRIPTS_DIR/sources/github-actions.sh
+
+gh pr create \
+  -B main \
+  -b "${pr_body}" \
+  -r tomthorogood \
+  -r goulter \
+  -r jdiverp \
+  --fill
+
+pr_number=$(gh pr list --json number -q '.[0].number')
+
+set_ci_output pull-request-number "$pr_number"
