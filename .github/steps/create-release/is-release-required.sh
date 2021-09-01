7892df35f611ed225420d391df0b3c267c47b2ac
Thomas Thorogood <goodtom@uw.edu>
2021-09-01 12:01:10 -0700

    Support 'semver-guidance:no-bump' to indicate a PR is a docs-only update (#98)
    * Support 'semver-guidance:no-bump' to indicate a PR is a docs-only update.
    
    * Don't create a new release if the version hasn't changed
    
    * [Bot] Update version to 2.0.1
    
    * Update doc to mention the 'no-bump' contingency
    
    * Install build scripts during configure-release job
    
    * Ensure quotes are stripped from git api output
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ .github/steps/create-release/is-release-required.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+source .build-scripts/sources/github-actions.sh
+
+current_version=$(poetry version -s)
+latest_release=$(curl -s https://api.github.com/repos/UWIT-IAM/uw-husky-directory/releases | jq '.[0].tag_name')
+latest_release=$(echo "${latest_release}" | sed 's|"||g')
+
+set_ci_output version "${current_version}"
+set_ci_output latest-release "${latest_release}"
+if [[ "${current_version}" == "${latest_release}" ]]
+then
+  set_ci_output release-required false
+else
+  set_ci_output release-required true
+fi
