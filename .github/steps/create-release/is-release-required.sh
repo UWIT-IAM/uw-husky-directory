62529105616d733d74bde243bb8bdcad18549df5
Tom Thorogood <tom@tomthorogood.com>
2021-09-01 11:26:21 -0700

    Ensure quotes are stripped from git api output

--- .github/steps/create-release/is-release-required.sh
+++ .github/steps/create-release/is-release-required.sh
@@ -4,6 +4,7 @@ source .build-scripts/sources/github-actions.sh
 
 current_version=$(poetry version -s)
 latest_release=$(curl -s https://api.github.com/repos/UWIT-IAM/uw-husky-directory/releases | jq '.[0].tag_name')
+latest_release=$(echo "${latest_release}" | sed 's|"||g')
 
 set_ci_output version "${current_version}"
 set_ci_output latest-release "${latest_release}"
