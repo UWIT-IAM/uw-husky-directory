c2400d92834112452dd97b6535bf435e54ded3fb
Tom Thorogood <tom@tomthorogood.com>
2021-09-08 15:38:35 -0700

    Blacken gunicorn.conf.py

--- husky_directory/gunicorn.conf.py
+++ husky_directory/gunicorn.conf.py
@@ -1,18 +1,19 @@
 import os
 from multiprocessing import cpu_count
 
+
 def worker_exit(worker, server):
     worker.log.info(f"Server {server} shutting down . . .")
 
 
 def max_workers():
     default_max = 2 * cpu_count() + 1
-    return os.environ.get('GUNICORN_MAX_WORKERS', default_max)
+    return os.environ.get("GUNICORN_MAX_WORKERS", default_max)
 
 
 def max_threads():
     default_max = 4
-    return os.environ.get('GUNICORN_MAX_THREADS', default_max)
+    return os.environ.get("GUNICORN_MAX_THREADS", default_max)
 
 
 max_requests = 1000
