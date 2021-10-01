2d0c3c59cc3821bb9fedfa3a79932de05a2fabe8
Tom Thorogood <tom@tomthorogood.com>
2021-10-01 12:45:07 -0700

    Use `gevent` as our gunicorn worker type
    Load testing against dev using `gthread`, `eventlet`, and `gevent` revealed gevent to be the best choice for this application, with a ~7% reduction in errors when running at/near the CPU limit for the cluster.

--- husky_directory/gunicorn.conf.py
+++ husky_directory/gunicorn.conf.py
@@ -1,7 +1,12 @@
-import os
-from multiprocessing import cpu_count
+from gevent import monkey
 
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+monkey.patch_all()
+
+import os  # noqa: E402
+from multiprocessing import cpu_count  # noqa: E402
+from prometheus_flask_exporter.multiprocess import (  # noqa: E402
+    GunicornInternalPrometheusMetrics,
+)
 
 if not os.environ.get("PROMETHEUS_MULTIPROC_DIR"):
     raise EnvironmentError("PROMETHEUS_MULTIPROC_DIR environment variable must be set!")
@@ -16,20 +21,14 @@ def max_workers():
     return os.environ.get("GUNICORN_MAX_WORKERS", default_max)
 
 
-def max_threads():
-    default_max = 4
-    return os.environ.get("GUNICORN_MAX_THREADS", default_max)
-
-
 def child_exit(server, worker):
     GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
 
 
 max_requests = 1000
 bind = "0.0.0.0:8000"
-worker_class = "gthread"
+worker_class = "gevent"
 workers = max_workers()
-threads = max_threads()
 loglevel = os.environ.get("GUNICORN_LOG_LEVEL", "DEBUG")
 reload = os.environ.get("FLASK_ENV") == "development"
 preload_app = os.environ.get("FLASK_ENV") != "development"
