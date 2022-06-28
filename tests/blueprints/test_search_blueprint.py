caf7f8d11c0b282eb491bbeca73e061eed866c2b
Tom Thorogood <tom@tomthorogood.com>
2022-06-28 10:32:33 -0700

    Optimize imports

--- tests/blueprints/test_search_blueprint.py
+++ tests/blueprints/test_search_blueprint.py
@@ -4,11 +4,10 @@ from datetime import datetime
 from typing import cast
 from unittest import mock
 
-from flask import Response
-from flask.testing import FlaskClient
-
 import pytest
 from bs4 import BeautifulSoup
+from flask import Response
+from flask.testing import FlaskClient
 from inflection import titleize
 from werkzeug import exceptions
 from werkzeug.local import LocalProxy
