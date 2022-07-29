8ddb58070d5dcc515a0c3aec3a5ac41c99ae42ce
Tom Thorogood <tom@tomthorogood.com>
2022-07-29 16:58:19 -0700

    More fixes to release workflow

--- .github/steps/create-release/is-release-required.sh
+++ .github/steps/create-release/is-release-required.sh
@@ -5,8 +5,8 @@ current_version=$(get_poetry_version)
 latest_release=$(curl -s https://api.github.com/repos/UWIT-IAM/uw-husky-directory/releases | jq '.[0].tag_name')
 latest_release=$(echo "${latest_release}" | sed 's|"||g')
 
-set_ci_output version "${current_version}"
-set_ci_output latest-release "${latest_release}"
+echo "::set-output name=version::${current_version}"
+echo "::set-output name=latest-release::${latest_release}"
 if [[ "${current_version}" == "${latest_release}" && "${GITHUB_REF}" == "refs/heads/main" ]]
 then
   echo "::set-output name=release-required::false"
