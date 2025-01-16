0e32ecf85839cb24fcdb56d59c5ecea947e27a45
soufianerafik <srafik@uw.edu>
2025-01-16 13:52:26 -0800

    chore(backend): Update old comment to replace it-wseval1 with wseval

--- husky_directory/models/enum.py
+++ husky_directory/models/enum.py
@@ -7,7 +7,7 @@ from enum import Enum
 class AffiliationState(Enum):
     """
     These are defined by PWS but only enumerated in their test UI:
-    https://it-wseval1.s.uw.edu/identity/v2/person
+    https://wseval.s.uw.edu/identity/v2/person
     """
 
     current = "current"
