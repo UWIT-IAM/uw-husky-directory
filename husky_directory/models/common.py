b7ca037509b13b6b77eb0b761255aee9c5f06285
Glen Goulter <37945000+goulter@users.noreply.github.com>
2026-04-07 14:26:08 -0700

    [GH-207] also GH-200 thru GH-206 - multiple phones and DAI (#209)
    * update dockerfile
    
    * various updates needed to get app to run locally and test properly again
    
    * Use the python-base image and install other deps to replace uw-saml-poetry
    
    * Show all the "phones" a user has on the summary screen. This does not include pagers, etc. Those are all still listed on the details page. If the user has no "phones" but does have "mobiles", mobiles will be shown.
    
    * formatting edits
    
    * add label to search form
    
    * Improve link color contrast so that links are distinguished from surrounding text in a way that does not rely on color. Color contrast was improved as suggested by dubbot.
    
    * Correct the dubbot item, Ensure select element has an accessible name. This was addressed by adding aria-label as suggested by dubbot.
    
    * Correct the dubbot issue Ensure <meta name="viewport"> does not disable text scaling and zooming. Removed the user-scalable=no as suggested.
    
    * Revert redis version to latest, and keep the new quotes around the command for redis ACL authentication
    
    * [Bot] Update version to 2.4.6
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- husky_directory/models/common.py
+++ husky_directory/models/common.py
@@ -5,6 +5,7 @@ the behavior itself is truly common. Otherwise, those behaviors
 should be declared in an inheriting subclass within the context of
 the respective service model.
 """
+
 from __future__ import annotations
 
 import re
